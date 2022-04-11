d249e18baac2711b35d6b8aea6a61fa5a4a4faf7
Matt McNaney <mcnaneym@appstate.edu>
2022-04-11 08:25:37 -0400

    Updates webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,22 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.jsx?/,
         include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react',]
-        }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
-    ]
+    ],
   },
 }
